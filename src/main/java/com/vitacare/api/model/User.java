4ea949dd13d5f4fe7acc2bed6cb1fb10cedc47a7
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2026-03-17 20:18:05 -0300

    fix: arrumando models e controller apenas para começo de testes

--- src/main/java/com/vitacare/api/model/User.java
+++ src/main/java/com/vitacare/api/model/User.java
@@ -1,11 +1,17 @@
 package com.vitacare.api.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.OffsetDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
@@ -14,11 +20,18 @@ import java.time.OffsetDateTime;
 @AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "uuid")
+    private UUID id;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
 
-    @Column(nullable = false, unique = true)
-    private String username;
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "role", nullable = false, length = 20)
+    private String role;
 
     @Column(name = "created_at", insertable = false, updatable = false)
     private OffsetDateTime createdAt;
